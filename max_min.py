3f1e32af6958f586bb285d72f6fee2ffbe20e1d7
shipra aggarwal <63389683+shipraagg@users.noreply.github.com>
2023-10-04 17:51:45 +0530

    Add files via upload

---
+++ max_min.py
@@ -0,0 +1,45 @@
+#3rd max element
+def thirdMax(list):
+    list.sort(reverse = True)
+    count = 1
+    previous = list[0]
+
+    for i in range(len(list)):
+        if list[i] != previous:
+            count = count + 1
+            previous = list[i]
+        if count == 3:
+            return list[i]
+    return list[0] 
+
+list = []
+n = int(input("Enter number of elements : "))
+for i in range(0, n):
+    ele = int(input())
+    list.append(ele) 
+ result=thirdMax(list)
+ print("third max element:",result)
+
+ #3rd min element
+
+ def thirdMin(list):
+    list.sort(reverse = False)
+    count = 1
+    previous = list[0]
+
+    for i in range(len(list)):
+        if list[i] != previous:
+            count = count + 1
+            previous = list[i]
+        if count == 3:
+            return list[i]
+    return list[0] 
+
+list = []
+n = int(input("Enter number of elements : "))
+for i in range(0, n):
+    ele = int(input())
+    list.append(ele) 
+ result=thirdMin(list)
+ print("third min element:",result)
+
